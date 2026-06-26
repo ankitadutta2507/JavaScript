dbf3e56a5186d73560cc32a4a19ecd587c0b8578
ankitadutta2507 <137138498+ankitadutta2507@users.noreply.github.com>
2026-06-26 16:49:16 +0530

    Validate age input for driving eligibility
    Added validation for negative age input in canIDrive.js.

--- canIDrive.js
+++ canIDrive.js
@@ -1,7 +1,11 @@
 let runAgain=true;
+
 while(runAgain) {
     let age = Number.parseInt(prompt("What is your age? "));
-    if (age>=18){
+    
+    if (age<0) {
+        console.error("age entered is negative");
+    } else if (age>=18){
         alert("You can Drive");
     } else if (age<18) {
         alert("You can't Drive");
