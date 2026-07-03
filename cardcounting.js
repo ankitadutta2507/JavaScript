843643b333a183ff84a62bb8c1fa7558e5912ac5
ankitadutta2507 <137138498+ankitadutta2507@users.noreply.github.com>
2026-07-03 14:22:14 +0530

    Implement card counting function in cardcounting.js

---
+++ cardcounting.js
@@ -0,0 +1,16 @@
+let count = 0;
+function cardCounter(card) {
+  if (card>=2 && card<=6) {
+    count++;
+  } else if (card == 10 || card == "J" || card == "Q" || card == "K" || card == "A") 
+  {
+    count--
+  } else {
+    count = count;
+  }
+if (count>0){
+  return `${count} Bet`;
+} else {
+  return `${count} Hold`;
+}
+}
