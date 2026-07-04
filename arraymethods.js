8e254e000d723d4595f6965fedd28bd5ba0abbe8
ankitadutta2507 <137138498+ankitadutta2507@users.noreply.github.com>
2026-07-04 05:56:44 +0530

    Add lunch menu management functions
    Implement functions to manage a lunch menu, including adding, removing, and displaying items.

---
+++ arraymethods.js
@@ -0,0 +1,55 @@
+let lunches = [];
+function addLunchToEnd(arr,item) {
+  arr.push(item);
+  console.log(`${item} added to the end of the lunch menu.`);
+  return arr;
+}
+
+function addLunchToStart(arr,item) {
+  arr.unshift(item);
+  console.log(`${item} added to the start of the lunch menu.`);
+  return arr;
+}
+
+function removeLastLunch(arr) {
+  if (arr.length ==0) {
+    console.log("No lunches to remove.");
+  } else {
+    
+    console.log(`${arr.pop()} removed from the end of the lunch menu.`);
+    return arr;
+}
+}
+
+
+
+
+
+
+function removeFirstLunch(arr) {
+  if (arr.length == 0) {
+    console.log("No lunches to remove.");
+  } else {
+  console.log(`${arr.shift()} removed from the start of the lunch menu.`);
+  return arr;
+  }
+}
+
+function getRandomLunch(arr) {
+  if (arr.length == 0) {
+    console.log("No lunches available.");
+  } else {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    console.log(`Randomly selected lunch: ${arr[randomIndex]}`);
+
+}
+}
+
+function showLunchMenu(arr) {
+  if (arr.length == 0) {
+    console.log("The menu is empty.");
+  } else {
+    console.log(`Menu items: ${arr.join(", ")}`);
+  }
+  
+}
