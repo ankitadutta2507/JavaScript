a673f280f671d27d5522fa9af3e7cdc95ccae9de
ankitadutta2507 <137138498+ankitadutta2507@users.noreply.github.com>
2026-07-08 07:33:37 +0530

    Created a function to Find Second Largest Number from Array in two loop solution

---
+++ findSecondLargestFromArr.js
@@ -0,0 +1,21 @@
+function findSecondLargest(arr) {
+  if (arr.length < 2) return undefined;
+
+  let largest = arr[0];
+  let secondLargest = -Infinity;
+
+  for (let element1 of arr) {
+    if (element1 > largest) {
+      largest = element1;
+    }
+  }
+
+  for (let element2 of arr) {
+    if (element2 < largest && element2 > secondLargest) {
+      secondLargest = element2;
+    }
+  }
+
+  return secondLargest === -Infinity ? undefined : secondLargest;
+}
+// TODO: Interview Follow-up: Solve this using only one traversal (single loop) while maintaining O(n) time and O(1) space.
