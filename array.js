5b299da3a0f0ca09f058f7a9a267a341a76bcd79
ankitadutta2507 <137138498+ankitadutta2507@users.noreply.github.com>
2026-08-08 11:46:07 +0530

    Implement various array manipulation functions

--- array.js
+++ array.js
@@ -96,9 +96,109 @@ if (found) {
 //______________________________________________________________________________
 
 // 49. Find the second-largest number.
+// //Start with both as very small values.
+// Go through the array one number at a time.
+// If the current number is greater than largest, move the old largest into secondLargest, then make the current number largest.
+// Otherwise, if the current number is greater than secondLargest but smaller than largest, update secondLargest.
+// After the loop, secondLargest contains the second-largest number.
+let arr = [23, 35, 47, 51, 60, 76];
+
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let num of arr) {
+    if (num > largest) {
+        secondLargest = largest;
+        largest = num;
+    } else if (num > secondLargest && num !== largest) {
+        secondLargest = num;
+    }
+}
+
+console.log("Second largest number is:", secondLargest);
+//______________________________________________________________________________
 // 50.Remove duplicate values.
+
+let arr = [25, 92, 37, 92, 92, 25, 33];
+let newArr = [];
+
+for (let num of arr){
+    if (!newArr.includes(num)){
+        newArr.push(num);
+    }
+}
+console.log(newArr);
+//________________________________________________________________________________
 // 51. Find duplicate values.
+let arr = [25, 92, 37, 92, 92, 25, 33];
+let newArr = [];
+let duplicateValues = [];
+for (let num of arr){
+    if (!newArr.includes(num)){
+        newArr.push(num);
+    } else if (!duplicateValues.includes(num)){
+        duplicateValues.push(num);
+    }
+}
+console.log(duplicateValues);
+/___________________________________________________________________________________
 // 52. Merge two arrays.
+
+let fruits = ["apple" , "banana", "cherry"];
+let veggies = ["cabbage", "potato", "carrot"];
+
+// let shoppingList = fruits.concat(veggies);
+// console.log(shoppingList);
+//merge with duplicates
+
+
+for (let item of veggies){
+    if (!fruits.includes(item)){
+        fruits.push(item);
+    }
+}
+//merge without duplicates
+console.log(fruits);
+/___________________________________________________________________________________
 // 53.Find common elements between two arrays.
+
+let GuestList1 = ["Ram", "Sam", "Jodu", "Mina"];
+let GuestList2 = ["Mina", "Tina", "Rina", "Sam"];
+let commonList = [];
+
+for (let guest of GuestList1){
+    if (GuestList2.includes(guest)){
+        commonList.push(guest);
+    }
+}
+console.log(commonList);
+//__________________________________________________________________________________________
 // 54. Find elements that exist in one array but not another.
+
+let empUsernames1 = ["johndoe", "janedoe", "asmith", "mjohnson", "kpatel", "emilyr", "davidk"];
+let empUsernames2 = ["robertc","sjenkins","asmith", "emilyr","davidk","ahassan","janedoe"];
+
+let uniqueUsernames1 = [];
+let uniqueUsernames2 = [];
+
+for (let name of empUsernames1){
+  if (!empUsernames2.includes(name)){
+    uniqueUsernames1.push(name);
+  }
+}
+
+for (let name of empUsernames2){
+  if (!empUsernames1.includes(name)){
+    uniqueUsernames2.push(name);
+  }
+}
+
+console.log("Unique usernames of list 1", uniqueUsernames1 );
+console.log("Unique usernames of list 2", uniqueUsernames2 );
+//__________________________________________________________________________________________
 // 55. Rotate an array left by one position.
+
+let arr = [23, 35, 47, 51, 60, 76];
+
+arr.push(arr.shift());
+console.log(arr);
