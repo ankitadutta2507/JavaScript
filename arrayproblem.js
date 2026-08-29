8dac4647cb8e8dbba6cc7f6d6b1e24183976d25f
ankitadutta2507 <137138498+ankitadutta2507@users.noreply.github.com>
2026-08-29 16:31:04 +0530

    Add utility functions for array operations

---
+++ arrayproblem.js
@@ -0,0 +1,9 @@
+const squareNumbers = (arr) => (arr.map(num => num*num));
+
+const filterEvenNumbers = (arr) => (arr.filter(num => num % 2 ===0));
+
+const sumPositiveNumbers = (arr) => (arr.filter(num => num > 0)).reduce((sum, current) => sum + current, 0);
+
+const getNames = (arr) => (arr.map(element => element.name));
+
+const findLongestWord = (arr) => (arr.reduce((longest,current) =>current.length > longest.length? current : longest));
